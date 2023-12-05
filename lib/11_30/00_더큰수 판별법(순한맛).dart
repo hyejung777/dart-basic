e134aa1e33e9fae826dee40a2210beecaa9d958a
hyejung <lstarsl1214@naver.com>
2023-12-05 09:04:55 +0900

    더 큰수 판별법(순한맛)

---
+++ lib/11_30/00_더큰수 판별법(순한맛).dart
@@ -0,0 +1,16 @@
+void main() {
+  print(solution(10, 3));
+}
+
+
+  String solution(int a, int b) {
+if (a > b) {
+return a.toStrinG();
+} else if (b > a);
+return a.toString();
+} else if (b > a){
+  return b.toString();
+}
+}
+return 'eq';
+}
